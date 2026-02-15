cf58b7ae5ae2f2ad49f84170d543e291392242cc
Puneet Arora <arorapuneet234@gmail.com>
2026-02-15 15:54:42 +0530

    Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -48,6 +48,13 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     })
@@ -68,7 +75,10 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUserFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(["firstName", "lastName"]);
+    })
+      .select(["firstName", "lastName"])
+      .skip(skip)
+      .limit(limit);
 
     res.json({ users });
   } catch (err) {
